391346430effd1dc2f0cbe32f048dc3c53e7f18d
Damilio <klever.cando12@gmail.com>
2025-08-01 19:40:28 -0500

    Cambio de carpetas para la raíz

--- main.js
+++ main.js
@@ -12,37 +12,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Imágenes en cascada
   const imagenes = [
-    "../imagenes/imagen.webp",
-    "../imagenes/imagen1.jpg",
-    "../imagenes/imagen2.jpg",
-    "../imagenes/imagen3.webp",
-    "../imagenes/imagen4.webp",
-    "../imagenes/imagen5.webp",
-    "../imagenes/imagen6.webp",
-    "../imagenes/imagen7.jpg",
-    "../imagenes/imagen8.webp",
-    "../imagenes/imagen9.webp",
-    "../imagenes/imagen10.webp",
-    "../imagenes/imagen11.webp",
-    "../imagenes/imagen12.jpg",
-    "../imagenes/imagen13.webp",
-    "../imagenes/imagen14.webp",
-    "../imagenes/imagen15.jpg",
-    "../imagenes/imagen16.webp",
-    "../imagenes/imagen17.webp",
-    "../imagenes/imagen18.webp",
-    "../imagenes/imagen19.webp",
-    "../imagenes/imagen20.webp",
-    "../imagenes/imagen21.webp",
-    "../imagenes/imagen22.webp",
-    "../imagenes/imagen23.webp",
-    "../imagenes/imagen24.webp",
-    "../imagenes/imagen25.webp",
-    "../imagenes/imagen26.webp",
-    "../imagenes/imagen27.webp",
-    "../imagenes/imagen28.webp",
-    "../imagenes/imagen29.webp",
-    "../imagenes/imagen30.webp"
+    "imagenes/imagen.webp",
+    "imagenes/imagen1.jpg",
+    "imagenes/imagen2.jpg",
+    "imagenes/imagen3.webp",
+    "imagenes/imagen4.webp",
+    "imagenes/imagen5.webp",
+    "imagenes/imagen6.webp",
+    "imagenes/imagen7.jpg",
+    "imagenes/imagen8.webp",
+    "imagenes/imagen9.webp",
+    "imagenes/imagen10.webp",
+    "imagenes/imagen11.webp",
+    "imagenes/imagen12.jpg",
+    "imagenes/imagen13.webp",
+    "imagenes/imagen14.webp",
+    "imagenes/imagen15.jpg",
+    "imagenes/imagen16.webp",
+    "imagenes/imagen17.webp",
+    "imagenes/imagen18.webp",
+    "imagenes/imagen19.webp",
+    "imagenes/imagen20.webp",
+    "imagenes/imagen21.webp",
+    "imagenes/imagen22.webp",
+    "imagenes/imagen23.webp",
+    "imagenes/imagen24.webp",
+    "imagenes/imagen25.webp",
+    "imagenes/imagen26.webp",
+    "imagenes/imagen27.webp",
+    "imagenes/imagen28.webp",
+    "imagenes/imagen29.webp",
+    "imagenes/imagen30.webp"
   ];
 
   const intervalo = 1000;
@@ -81,8 +81,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   setInterval(cascadaImagenes, intervalo);
 
+  // Reproductor de música
   const audio = document.getElementById("audio");
-  const canciones = ["cancion1.mp3", "cancion2.mp3", "cancion3.mp3", "cancion4.mp3", "cancion5.mp3", "cancion6.mp3", "cancion7.mp3", "cancion8.mp3"];
+  const canciones = [
+    "musica/cancion1.mp3",
+    "musica/cancion2.mp3",
+    "musica/cancion3.mp3",
+    "musica/cancion4.mp3",
+    "musica/cancion5.mp3",
+    "musica/cancion6.mp3",
+    "musica/cancion7.mp3",
+    "musica/cancion8.mp3"
+  ];
   let indice = 0;
   let reproduciendo = false;
 
@@ -92,20 +102,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const imgToggle = btnToggle.querySelector("img");
 
   function cargarCancion() {
-    audio.src = `../musica/${canciones[indice]}`;
+    audio.src = canciones[indice];
   }
 
   function reproducir() {
     cargarCancion();
     audio.play();
     reproduciendo = true;
-    imgToggle.src = "../imagenes/pause.png";
+    imgToggle.src = "imagenes/pause.png";
   }
 
   function pausar() {
     audio.pause();
     reproduciendo = false;
-    imgToggle.src = "../imagenes/start.png";
+    imgToggle.src = "imagenes/start.png";
   }
 
   btnToggle.addEventListener("click", () => {
@@ -131,38 +141,39 @@ document.addEventListener("DOMContentLoaded", () => {
     reproducir();
   });
 
+  // Mensajes que se escriben
   const mensaje = document.getElementById("mensaje-escrito");
   const frases = [
-  "Hola mi vida, quiero desearte un Feliz Cumpleaños.",
-  "Deseo que este día esté lleno de alegrías para ti, Kiara.",
-  "Me alegra saber que puedo compartir este cumpleaños contigo.",
-  "Incluso si es a la distancia, estoy feliz de estar acá.",
-  "Cada día que pasa me enamoro mucho más de ti.",
-  "Siempre voy a agradecer a la vida por haberte conocido.",
-  "Estoy feliz de poder verte crecer, de verte madurar.",
-  "Pero recuerda, no hay prisa por crecer.",
-  "No tengas prisa por madurar. Tú eres tú.",
-  "Tú eres especial como eres, no necesitas cambiar.",
-  "Te amo mucho, mi amor, mi vida, mi todo.",
-  "Feliz cumpleaños mi niña consentida, te amo 💗🎂"
-];
+    "Hola mi vida, quiero desearte un Feliz Cumpleaños.",
+    "Deseo que este día esté lleno de alegrías para ti, Kiara.",
+    "Me alegra saber que puedo compartir este cumpleaños contigo.",
+    "Incluso si es a la distancia, estoy feliz de estar acá.",
+    "Cada día que pasa me enamoro mucho más de ti.",
+    "Siempre voy a agradecer a la vida por haberte conocido.",
+    "Estoy feliz de poder verte crecer, de verte madurar.",
+    "Pero recuerda, no hay prisa por crecer.",
+    "No tengas prisa por madurar. Tú eres tú.",
+    "Tú eres especial como eres, no necesitas cambiar.",
+    "Te amo mucho, mi amor, mi vida, mi todo.",
+    "Feliz cumpleaños mi niña consentida, te amo 💗🎂"
+  ];
 
   let fraseIndex = 0; 
   let letraIndex = 0;
 
-function escribirFrase() {
-  if (letraIndex < frases[fraseIndex].length) {
-    mensaje.textContent += frases[fraseIndex].charAt(letraIndex);
-    letraIndex++;
-    setTimeout(escribirFrase, 50);
-  } else {
-    setTimeout(() => {
-      fraseIndex = (fraseIndex + 1) % frases.length;
-      mensaje.textContent = "";
-      letraIndex = 0;
-      escribirFrase();
-    }, 2000);
+  function escribirFrase() {
+    if (letraIndex < frases[fraseIndex].length) {
+      mensaje.textContent += frases[fraseIndex].charAt(letraIndex);
+      letraIndex++;
+      setTimeout(escribirFrase, 50);
+    } else {
+      setTimeout(() => {
+        fraseIndex = (fraseIndex + 1) % frases.length;
+        mensaje.textContent = "";
+        letraIndex = 0;
+        escribirFrase();
+      }, 2000);
+    }
   }
-}
-escribirFrase();
+  escribirFrase();
 });
